8048fa4c33a98e098953f97417943d5989265e4a
Sittipol Khamrueangsri <sittipol.kha@msu.ac.th>
2017-10-30 16:00:52 +0700

    commit

---
+++ sugarcane-ios/Classes/Knowledge/KnowledgeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  KnowledgeViewController.swift
+//  sugarcane-ios
+//
+//  Created by Riseplus on 10/30/17.
+//  Copyright © 2017 Riseplus. All rights reserved.
+//
+
+import UIKit
+
+class KnowledgeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ sugarcane-ios/Extension/UITabBarItem.swift
@@ -0,0 +1,18 @@
+//
+//  UITabBarItem.swift
+//  sugarcane-ios
+//
+//  Created by Riseplus on 10/30/17.
+//  Copyright © 2017 Riseplus. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITabBarItem {
+    
+    func setTitleItem() -> Void {
+        UITabBarItem.init(title: "คลังความรู้", image: UIImage(), tag: 0)
+    
+    }
+}
