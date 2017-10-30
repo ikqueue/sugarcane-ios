68f13fe4a1c146083659ad1e54d235ca392084d2
Sittipol Khamrueangsri <sittipol.kha@msu.ac.th>
2017-10-30 14:41:59 +0700

    Initial Commit

---
+++ sugarcane-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sugarcane-ios
+//
+//  Created by Riseplus on 10/30/17.
+//  Copyright © 2017 Riseplus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
